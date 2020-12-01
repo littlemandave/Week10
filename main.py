200aa718515102630b6bce69b757bcf349fc21af
Dave Johnson <dave.johnson4@pcc.edu>
2020-11-30 22:36:48 -0800

    2 plots

--- main.py
+++ main.py
@@ -13,97 +13,66 @@ def processWind(direction, speed):
     pass
 
 def main():
-    numDashes = 10
+    numDashes = 40
     print()
     print('-' * numDashes)
     print('--- numpy, pandas, matplotlib ---')
     print('-' * numDashes)
     print()
 
-    numPoints = 50
+    numPoints = 5000
 
     # Get the last numPoints wind direction (field1) and speed (field2) samples
+    dirName = 'Direction(deg)'
+    speedName = 'Velocity(mph)'
     getter = wg.WeatherGetter()
     windDataFrame = getter.getData(1, numPoints)
     speedDF = getter.getData(2, numPoints)
+
+    # Get rid of the 'entry_id' column in both dataframes
     del windDataFrame['entry_id']
     del speedDF['entry_id']
+
+    # Insert the wind velocity column as 'field2'
+    windDataFrame.insert( loc=len(windDataFrame.columns), column='field2', value=speedDF['field2'])
+
+    # Rename the columns to something that makes sense
+    windDataFrame.rename(columns={'created_at': 'Date/Time', 'field1': dirName, 'field2': speedName}, inplace=True)
     print(windDataFrame)
-    print(speedDF)
 
-    #windDataFrame.set_index('created_at').join(speedDF.set_index('created_at'))
-    print('\nJoining...\n')
-    # windDataFrame.merge(speedDF['field2'])
-    windDataFrame.join(speedDF['field2'], how='inner', lsuffix='_x', rsuffix='_y')
-    print(windDataFrame)
-#    windDataFrame.join(speedDF)
+    # Plot it, both polar and x vs y
+    title = "Plot of Wind direction vs. Speed in Natick, MA using the past {} data points".format(numPoints)
+    ax = plt.subplot(211, projection='polar')
+    ax.set_theta_offset(np.pi / 2)
+    tickLabels = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE']
+    tickValues = []
+    for i in range(8):
+        tickValues.append(i*(np.pi/4))
+    ax.set_xticks(tickValues)
+    ax.set_xticklabels(tickLabels)
+    plt.title(title)
+    # Plot polar to show direction and speed
+    # convert dirData to radians for use in a polar plot. Note we're using numpy-style notation here:
+    # operation is performed on the entire array
+    # rad = deg * pi/180, but we need to reverse direction for polar plot, so rad = -deg*pi/180
+    radData = ((-windDataFrame[dirName] * np.pi) / 180)
+    # add 2pi to make sure it's positive
+    radData += (2*np.pi)
+    plt.plot(radData, windDataFrame[speedName], 'g.')
 
-#     # index = 'field' + str(2)  # creating column index 'fieldX' for the data
-#     # speedSeries = speedDF[index]  # indexing into the file for the column index
-#     #
-#     # # Append the column to the dataframe
-#     # windDataFrame[index] = speedSeries
-#
-#
-#     # print(speedData)
-#     # print(dirData)
-#
-#     # Plot it
-#     title = "Plot of Wind direction vs. Speed in Natick, MA using the past {} data points".format(numPoints)
-#
-#     # convert dirData to radians for use in a polar plot. Note we're using numpy-style notation here:
-#     # operation is performed on the entire array
-#     # rad = deg * pi/180, but we need to reverse direction for polar plot, so rad = -deg*pi/180
-#     radData = (((dirData * -1) * np.pi) / 180)
-#     # add 2pi to make sure it's positive
-#     radData += (2 * np.pi)
-#
-# #    plt.polar(radData, speedData, 'rx')
-#     plt.title(title)
-#     # plt.xlabel('Speed (mph)')
-#     # plt.ylabel('Direction')
-#     plt.plot(speedData, dirData, 'rx')
-#
-#     plt.show()
+    # Then plot linear
+    ax2 = plt.subplot(212)
+    plt.xlabel(speedName)
+    plt.ylabel(dirName)
+    plt.plot(windDataFrame[speedName], windDataFrame[dirName], 'rx')
+    tickLabels = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    tickValues = [0, 45, 90, 135, 180, 225, 270, 315]
+    ax2.set_yticks(tickValues)
+    ax2.set_yticklabels(tickLabels)
 
-    print('Done!')
+    plt.show()
+    print('\nDone!')
 
 
 if __name__ == '__main__':
     main()
-
-    # def plotData(self, series, title = '', xLabel = '', yLabel = '', legend = [], fmt = ''):
-    #     plt.plot(series)
-    #
-    #     # Step 4 Add details like title, labels, legend
-    #     plt.title(title)
-    #     plt.xlabel(xLabel)
-    #     plt.ylabel(yLabel)
-    #     plt.legend(legend)
-    #
-    # def plotDoubleData(self, series1, series2, title = '', xLabel = '', yLabel = '', legend = [], fmt = ''):
-    #     plt.plot(series1, series2)
-    #
-    #     # Step 4 Add details like title, labels, legend
-    #     plt.title(title)
-    #     plt.xlabel(xLabel)
-    #     plt.ylabel(yLabel)
-    #     plt.legend(legend)
-    #
-    # def plotPolar(self, series1, series2, title = '', xLabel = '', yLabel = '', legend = [], fmt = ''):
-    #     plt.polar(series1, series2, fmt)
-    #
-    #     # for i in range(len(series1)):
-    #     #     plt.polar(series1[i], series2[i])
-    #
-    #     # Step 4 Add details like title, labels, legend
-    #     plt.title(title)
-    #     plt.xlabel(xLabel)
-    #     plt.ylabel(yLabel)
-    #     plt.legend(legend)
-    #
-    # def showPlots(self):
-    #     # Step 5 - Show Plot and Save if needed
-    #     plt.savefig("weather.png", dpi=300, bbox_inches="tight")
-    #     plt.show()
-    #
